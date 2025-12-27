6ada243dc5bb8c8f2b1d5f53bdaacb1b8a83535d
Arda Kemiksiz <eldeirun06@gmail.com>
2025-12-27 22:10:11 +0300

    Rectangle Area Calculator

---
+++ Rectangle Area Calculator/RACalc.py
@@ -0,0 +1,8 @@
+print("Welcome to the Rectangle Area Calculator")
+
+side1 = float(input("Please input the first side's length: "))
+side2 = float(input("Please input the second side's length: "))
+
+area = side1 * side2
+
+print(f"The area of the defined rectangle is {area}")
