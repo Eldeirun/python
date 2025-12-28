9dfc9061d00b7b985a342147fb635ec900a33009
Arda Kemiksiz <eldeirun06@gmail.com>
2025-12-28 14:14:05 +0300

    Madlibs game

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,6 @@
+subject = input("Who is the story about?:")
+time = input("At what time does the story occur?:")
+act1 = input(f"What did {subject} first do?:")
+act2 = input(f"What did {subject} later do?:")
+
+print(f"Today {subject} woke up at {time} and {act1}, later he/she {act2}")
